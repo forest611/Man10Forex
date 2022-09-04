2fd900cf544376d353b3bc20b616b9ed1911c78b
forest611 <forest611328@gmail.com>
2022-09-04 12:55:12 +0900

    表示修正

--- src/main/java/red/man10/man10forex/forex/Command.kt
+++ src/main/java/red/man10/man10forex/forex/Command.kt
@@ -2,6 +2,8 @@ package red.man10.man10forex.forex
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.event.HoverEvent
+import net.kyori.adventure.text.event.HoverEventSource
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -49,40 +51,57 @@ object Command :CommandExecutor{
                 val profitColor = if (allProfit<0) "§4§l" else if (allProfit>0) "§b§l" else "§f§l"
                 val percentColor =  if (percent==0.0) "§f§l" else if (percent< lossCutPercent*1.5) "§4§l" else if (percent< lossCutPercent*2.0) "§6§l"  else "§f§l"
 
+                val percentMsg = Component.text("${prefix}${percentColor}維持率:${String.format("%,.3f", percent)}%")
+                    .hoverEvent(HoverEvent.showText(Component.text("§c§l維持率が20.0%を下回ると、ポジションが強制的にイグジットされます")))
+
                 sender.sendMessage("${prefix}§e§l=============[Man10Trader(MT10)]=============")
                 sender.sendMessage("${prefix}銀行残高:${String.format("%,.0f", bank)}")
                 sender.sendMessage("${prefix}有効金額:${String.format("%,.0f", margin)}")
                 sender.sendMessage("${prefix}${profitColor}評価額:${String.format("%,.0f", allProfit)}")
-                sender.sendMessage("${prefix}${percentColor}維持率:${String.format("%,.3f", percent)}%")
+                sender.sendMessage(percentMsg)
                 sender.sendMessage("${prefix}===保有ポジション(クリックしてイグジット(利益確定))===")
 
                 list.forEach {
 
                     val profit = profit(it)
 
-                    val eColor = (if (it.buy) "§a§lB" else "§c§lS") + " ${it.lots}ロット "
-                    val pColor = if (profit>0.0) "§b§l${String.format("%,.0f", profit)}円" else if(profit<0.0) "§4§l${String.format("%,.0f", profit)}円" else "§f§l${String.format("%,.0f", profit)}円"
-                    val start = "O:${String.format("%,.3f", it.entryPrice)} "
-
+                    val lots = (if (it.buy) "§a§lB" else "§c§lS") + " ${it.lots}ロット "
+                    val openPrice = "O:${String.format("%,.3f", it.entryPrice)} "
+                    val profitText = if (profit>0.0) "§b§l${String.format("%,.0f", profit)}円" else if(profit<0.0) "§4§l${String.format("%,.0f", profit)}円" else "§f§l${String.format("%,.0f", profit)}円"
                     val diff = " (${String.format("%,.1f", Forex.diffPips(it))}Pips)"
 
-                    val msg = Component.text(prefix+eColor+start+pColor+diff).
-                    clickEvent(ClickEvent.runCommand("/mfx exit ${it.positionID}"))
+                    val positionDataText = "§7§lポジション情報\n" +
+                            "${if (it.buy) "§a§l買" else "§c§l売"}ポジション\n" +
+                            "§7ロット数:§l${it.lots}\n" +
+                            "§7オープン価格:§l${String.format("%,.3f", it.entryPrice)}"
+
+                    val msg = Component.text(prefix+lots+openPrice+profitText+diff)
+                        .clickEvent(ClickEvent.runCommand("/mfx exit ${it.positionID}"))
+                        .hoverEvent(HoverEvent.showText(Component.text(positionDataText)))
 
                     val showTP = " §a§n[TP${if (it.tp!=0.0) "(${String.format("%,.3f", it.tp)})" else ""}]"
                     val showSL = " §c§n[SL${if (it.sl!=0.0) "(${String.format("%,.3f", it.sl)})" else ""}]"
 
-                    val compTP = Component.text(showTP).clickEvent(ClickEvent.suggestCommand("/mfx tp ${it.positionID} "))
-                    val compSL = Component.text(showSL).clickEvent(ClickEvent.suggestCommand("/mfx sl ${it.positionID} "))
+                    val compTP = Component.text(showTP)
+                        .clickEvent(ClickEvent.suggestCommand("/mfx tp ${it.positionID} "))
+                        .hoverEvent(HoverEvent.showText(Component.text("§a自動で利益を確定する価格を設定します")))
+                    val compSL = Component.text(showSL)
+                        .clickEvent(ClickEvent.suggestCommand("/mfx sl ${it.positionID} "))
+                        .hoverEvent(HoverEvent.showText(Component.text("§c自動で損失を確定する価格を設定します")))
 
                     sender.sendMessage(msg.append(compTP).append(compSL))
 
                 }
 
                 val prefix = Component.text(prefix)
-                val sellButton = Component.text("§c§l§n[売る]").clickEvent(ClickEvent.suggestCommand("/mfx sell "))
+                val sellButton = Component.text("§c§l§n[売る]")
+                    .clickEvent(ClickEvent.suggestCommand("/mfx sell "))
+                    .hoverEvent(HoverEvent.showText(Component.text("§c現在価格より下回ったら利益がでます\n§c/mfx sell <ロット数>(0.01〜1000)")))
                 val space = Component.text("    ")
-                val buyButton = Component.text("§a§l§n[買う]").clickEvent(ClickEvent.suggestCommand("/mfx buy "))
+                val buyButton = Component.text("§a§l§n[買う]")
+                    .clickEvent(ClickEvent.suggestCommand("/mfx buy "))
+                    .hoverEvent(HoverEvent.showText(Component.text("§a現在価格より上回ったら利益がでます\n§a/mfx buy <ロット数>(0.01〜1000)")))
+
 
                 sender.sendMessage(prefix.append(sellButton).append(space).append(buyButton))
 
